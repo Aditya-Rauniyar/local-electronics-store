aab3da75c367497c34f4b43bfc137b7695c5816e
Aditya Rauniyar <aditya.2327cs1230@kiet.edu>
2026-02-09 16:03:30 +0530

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Analytics} from '@vercel/analytics/react'
 
 // App bootstrap (single-page site)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
+
   </React.StrictMode>
 )
